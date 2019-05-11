a8f2c47dfe172cca6cb77211f1c162afd74097ef
Amanda Joan <amandajoanski@gmail.com>
2019-05-11 13:25:08 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar';
+import Header from './components/header';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/home'
+import Login from './pages/login'
 
-function App() {
+function Auth(){
+  //does google auth call
+  return 'Amanda'
+}
+
+export function API(){
+  //logic
+  return console.log('example')
+}
+
+ function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar name={Auth()} />
+     <BrowserRouter>
+    
+     <Route exact path="/" component={Login} />
+     <Route exact path="/home" component={Home} />
+    
+     </BrowserRouter>
     </div>
   );
 }
